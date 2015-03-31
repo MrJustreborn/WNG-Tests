aa179dd712acd211e108a31aa8653d9058475dbe
MrJustreborn <rene.lottes@gmx.de>
2015-03-31 18:11:09 +0200

    MapLoader_v2
    neuer MapLoader, noch ohne ausimplementieren Methoden
    Datei soll übersichtlicher und einfacher werden -> bessere struktur
    kleine REAME für die Maps hinzugefügt, is nur noch etwas unzureichend definiert.

---
+++ Maps/README.md
@@ -0,0 +1,10 @@
+#Maps
+>V0.0.2
+
+Maps haben den Ursprung bei X=0 und Y=0.
+
+Gänge befinden sich bei Z=0 und Z=1
+
+Ein vollständig abgebautes Teilstück für ein Gang hat somit die größe 2x2x2.
+
+Höhlen fangen bei Z=-2 an. (Z=-1 ist die Wand bei einem Gang)
---
+++ Scripts/MapLoader_v2.gd
@@ -0,0 +1,106 @@
+#****************************************
+# MapLoader_v2.gd
+#
+#
+#
+#
+#
+#****************************************
+
+const ERR_MAP_NOT_SET = -1
+const ERR_PATH_NOT_VALID = -2
+
+var material = null		#FixedMaterial
+var map = null			#GridMap
+var navi = null			#Vector3Array
+var naviMesh = null		#SurfaceTool -> Mesh
+
+var navMeshInst = null	#NavigationMeshInstance
+var meshInst = null		#MeshInstance
+
+#****************************************
+#	Init
+#****************************************
+func setMaterial(mat):
+	self.material = mat
+
+func setMap(gridMap):
+	self.map = gridMap
+
+#****************************************
+#	Parser
+#****************************************
+func loadMap(path):
+	if(map == null):
+		return ERR_MAP_NOT_SET
+	if!(File.new().file_exists(path)):
+		return ERR_PATH_NOT_VALID
+	var file = XMLParser.new()
+	file.open(path)
+	
+	pass
+
+func _parse_XYZ():
+	pass
+
+#****************************************
+#	getTest	1
+#****************************************
+func getNaviMesh():
+	if(naviMesh == null):
+		self._generateNaviMesh()
+	return naviMesh
+
+func getNaviVertices():
+	if(navi == null):
+		self._generateNaviVertices()
+	return navi
+
+#****************************************
+#	getTest	2
+#****************************************
+func getNavigationMeshInstance():
+	return navMeshInst
+
+func getMeshInstance():
+	return meshInst
+
+#****************************************
+#	update (is bei getTest 2 sinnvoll)
+#****************************************
+func update():
+	pass
+
+#****************************************
+#	Berechnung vom NavigationMesh
+#****************************************
+func _generateNaviMesh():
+	if(navi == null):
+		self._generateNaviVertices()
+	var up = Vector3(0, 1, 0)
+	# PRIMITIVE_POINTS = 0 - Render array as points (one vertex equals one point).
+	# PRIMITIVE_LINES = 1 - Render array as lines (every two vertices a line is created).
+	# PRIMITIVE_LINE_STRIP = 2 - Render array as line strip.
+	# PRIMITIVE_LINE_LOOP = 3 - Render array as line loop (like line strip, but closed).
+	# PRIMITIVE_TRIANGLES = 4 - Render array as triangles (every three vertices a triangle is created).
+	# PRIMITIVE_TRIANGLE_STRIP = 5 - Render array as triangle strips.
+	# PRIMITIVE_TRIANGLE_FAN = 6 - Render array as triangle fans.
+	naviMesh = SurfaceTool.new()
+	naviMesh.begin(naviMesh.PRIMITIVE_TRIANGLES)
+	if(material):
+		naviMesh.set_material(material)
+	for i in range(0,navi.size(),3):
+		naviMesh.add_uv(Vector2(0, 1))
+		naviMesh.add_normal(up)
+		naviMesh.add_vertex(navi.get(i))
+		naviMesh.add_uv(Vector2(1, 0))
+		naviMesh.add_normal(up)
+		naviMesh.add_vertex(navi.get(i+1))
+		naviMesh.add_uv(Vector2(1, 1))
+		naviMesh.add_normal(up)
+		naviMesh.add_vertex(navi.get(i+2))
+	naviMesh = naviMesh.commit()
+
+func _generateNaviVertices():
+	navi = Vector3Array()
+	pass
